480822c3d496708e4a4ce51c9e7165d7dec443b9
Lisa <elisaveta.bagaeva@autodesk.com>
2017-01-30 14:36:48 +0400

    Remuve extra conditions, update condition of while, add variable a

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,38 +1,27 @@
 public class BinarySearch {
 
 	public static Integer search(Integer[] sequance, Integer key) {
-
-		InsertionSort.sort(sequance);
-		int n = sequance.length;
 		int lb = 0;
-		int ub = n - 1;
+		int ub = sequance.length - 1;
+		int a;
 
-		while (ub - lb != 1) {
-			if (sequance[(ub + lb) / 2] == key)
-				return (ub + lb) / 2;
-			if (sequance[lb] == key)
-				return lb;
-			if (sequance[ub] == key)
-				return ub;
-			if (sequance[(ub + lb) / 2] > key)
-				ub = (ub + lb) / 2;
+		while (ub >= lb) {
+			a = (ub - lb) / 2;
+			if (sequance[a + lb] == key)
+				return a + lb;
+			if (sequance[a + lb] > key)
+				ub = a + lb - 1;
 			else
-				lb = (ub + lb) / 2;
+				lb = a + lb + 1;
 		}
 
-		if (sequance[lb] == key)
-			return lb;
-		if (sequance[ub] == key)
-			return ub;
 		return -1;
 	}
 
-	public static void main(String[] argc) {
-		Integer[] seq = { 4, 3, 6, 1, 7, 8, 1, 11, 9, 0 };
-		int key = 7;
-
-		System.out.println("Index = " + BinarySearch.search(seq, key));
-		for (int i = 0; i < seq.length; i++)
-			System.out.print(seq[i] + " ");
+	public static void main(String argv[]) {
+		Integer key = 4;
+		Integer[] seq = { 1, 2, 4, 40, 50, 60, 70 };
+		System.out.println(BinarySearch.search(seq, key));
 	}
+
 }
